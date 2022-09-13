fa4552d26b65f496b7bddb5070b78e243d73a194
mirzavic4869 <avicenamirzam@gmail.com>
2022-09-13 11:23:05 +0700

    setting layout and font

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from "react";
 import "./App.css";
+import Hasil from "./components/Hasil";
+import ListCategories from "./components/ListCategories";
+import Navbar from "./components/Navbar";
+import { Row, Col, Container } from "react-bootstrap";
 
 function App() {
 	return (
 		<div>
-			<div>Hello</div>
+			<Navbar />
+			<div className="mt-3">
+				<Container fluid>
+					<Row>
+						<ListCategories />
+						<Col>
+							<h4>
+								<strong>Daftar Produk</strong>
+								<hr />
+							</h4>
+						</Col>
+						<Hasil />
+					</Row>
+				</Container>
+			</div>
 		</div>
 	);
 }
---
+++ src/components/ListCategories.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Col } from "react-bootstrap";
+
+const ListCategories = () => {
+	return (
+		<Col md={3} mt="2">
+			<h4>
+				<strong>List Categories</strong>
+			</h4>
+			<hr />
+		</Col>
+	);
+};
+
+export default ListCategories;
