ab06200eb32d6bca3f1b26859941bbdf32768811
mirzavic4869 <avicenamirzam@gmail.com>
2022-09-16 14:58:31 +0700

    get all product

--- src/components/Menus.jsx
+++ src/components/Menus.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { Col } from "react-bootstrap";
 import Card from "react-bootstrap/Card";
+import { numberWithCommas } from "../utils/utils";
 
 const Menus = ({ menu }) => {
 	return (
-		<Col>
-			<Card style={{ width: "18rem" }}>
-				<Card.Img variant="top" src="holder.js/100px180" />
+		<Col md={4} sm={6} className="mb-4">
+			<Card className="shadow">
+				<Card.Img variant="top" src={"assets/assets/images/" + menu.category.nama.toLowerCase() + "/" + menu.gambar} />
 				<Card.Body>
-					<Card.Title>Card Title</Card.Title>
-					<Card.Text>Some quick example text to build on the card title and make up the bulk of the card's content.</Card.Text>
+					<Card.Title>
+						{menu.nama} ({menu.kode})
+					</Card.Title>
+					<Card.Text>Rp {numberWithCommas(menu.harga)}</Card.Text>
 				</Card.Body>
 			</Card>
 		</Col>
