8758985ec204a52f5d5cc74c0db6aae1d6c60770
mirzavic4869 <avicenamirzam@gmail.com>
2022-09-26 09:26:08 +0700

    sweet alert

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Menus from "./components/Menus";
 import { Row, Col, Container } from "react-bootstrap";
 import { API_URL } from "./utils/constants";
 import axios from "axios";
+import swal from "sweetalert";
 
 export default class App extends Component {
 	constructor(props) {
@@ -14,6 +15,7 @@ export default class App extends Component {
 		this.state = {
 			menus: [],
 			categoryYangDipilih: "Makanan",
+			keranjang: [],
 		};
 	}
 
@@ -23,6 +25,16 @@ export default class App extends Component {
 			.then((res) => {
 				const menus = res.data;
 				this.setState({ menus });
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+
+		axios
+			.get(API_URL + "keranjangs")
+			.then((res) => {
+				const keranjangs = res.data;
+				this.setState({ keranjangs });
 			})
 			.catch((error) => {
 				console.log(error);
@@ -46,23 +58,75 @@ export default class App extends Component {
 			});
 	};
 
+	masukKeranjang = (value) => {
+		axios
+			.get(API_URL + "keranjangs?product.id=" + value.id)
+			.then((res) => {
+				if (res.data.length === 0) {
+					const keranjang = {
+						jumlah: 1,
+						total_harga: value.harga,
+						product: value,
+					};
+					axios
+						.post(API_URL + "keranjangs", keranjang)
+						.then((res) => {
+							swal({
+								title: "Sukses Masuk Keranjang!",
+								text: "Sukses Masuk Keranjang " + keranjang.product.nama,
+								icon: "success",
+								button: false,
+								timer: 1500,
+							});
+						})
+						.catch((error) => {
+							console.log(error);
+						});
+				} else {
+					const keranjang = {
+						jumlah: res.data[0].jumlah + 1,
+						total_harga: res.data[0].total_harga + value.harga,
+						product: value,
+					};
+
+					axios
+						.put(API_URL + "keranjangs/" + res.data[0].id, keranjang)
+						.then((res) => {
+							swal({
+								title: "Sukses Masuk Keranjang!",
+								text: "Sukses Masuk Keranjang " + keranjang.product.nama,
+								icon: "success",
+								button: false,
+								timer: 1500,
+							});
+						})
+						.catch((error) => {
+							console.log(error);
+						});
+				}
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	};
+
 	render() {
-		const { menus } = this.state;
+		const { menus, categoryYangDipilih, keranjangs } = this.state;
 		return (
 			<div>
 				<Navbar />
 				<div className="mt-3">
 					<Container fluid>
 						<Row>
-							<ListCategories changeCategory={this.changeCategory} categoryYangDipilih={this.categoryYangDipilih} />
+							<ListCategories changeCategory={this.changeCategory} categoryYangDipilih={categoryYangDipilih} />
 							<Col>
 								<h4>
 									<strong>Daftar Produk</strong>
 								</h4>
 								<hr />
-								<Row>{menus && menus.map((menu) => <Menus key={menu.id} menu={menu} />)}</Row>
+								<Row>{menus && menus.map((menu) => <Menus key={menu.id} menu={menu} masukKeranjang={this.masukKeranjang} />)}</Row>
 							</Col>
-							<Hasil />
+							<Hasil keranjangs={keranjangs} {...this.props} />
 						</Row>
 					</Container>
 				</div>
